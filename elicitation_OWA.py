e10263776c154d28c4b7e77ea83dc524d254b49a
KohlerHECTOR <>
2022-01-22 16:55:39 +0100

    tmp

--- elicitation_OWA.py
+++ elicitation_OWA.py
@@ -12,7 +12,7 @@ def main_(X):
 		print("P : ", P)
 		print("x y : ", x, " ", y)
 		z = input("x or y ?")
-		
+
 		if z=='x':
 			P.append((x,y))
 		else:
@@ -24,7 +24,7 @@ def main_(X):
 def CSS(X,P):
 	print("IN CSS")
 	return MMR(X,P)
-	
+
 def MMR(X,P):
 	print("\n\nIN MMR")
 	min_ = 10e3
@@ -33,13 +33,13 @@ def MMR(X,P):
 	print("X : ", X)
 	for x in X :
 		y, valeur = MR(x, X, P)
-		
+
 		print("~~ x : ", x, "  ", valeur)
 		#input("x")
 		if valeur < min_ :
 			print(valeur, "<", min_)
 			#input("valeur < min")
-			min_ = valeur 
+			min_ = valeur
 			x_ = x
 	print(x_, "  ", min_)
 	#input("#### MMR return x, min")
@@ -59,7 +59,7 @@ def MR(x, X, P):
 			max_ = valeur
 			y_ = y
 	print(y_, "  ", max_)
-	#input("#### MR return y, max") 
+	#input("#### MR return y, max")
 	return y_, max_
 
 def PMR_owa(x,y,P):
@@ -67,16 +67,16 @@ def PMR_owa(x,y,P):
 	x_rearr = np.sort(x)
 	y_rearr = np.sort(y)
 	return PMR_ws(x_rearr,y_rearr,P)
-	
+
 #Implementation de PMR par Programme Linéaire
 def PMR_ws(x,y,P):
 	print("IN PMR_ws")
-	
+
 	nbvar = len(x)
 	columns = np.arange(nbvar, dtype=int)
-	
+
 	print("vector columns : ", columns)
-	
+
 	#create model
 	m = Model("PMR")
 
@@ -84,15 +84,15 @@ def PMR_ws(x,y,P):
 	w = []
 	for i in columns:
 		w.append(m.addVar(vtype=GRB.CONTINUOUS, name="w%d" %(i+1)))
-		
+
 	print(w)
 	#integration of new variable
 	m.update()
-	
+
 	#objective function
 	w = np.array(w)
 	obj = (w @ y - w @ x)
-	
+
 	m.setObjective(obj, GRB.MAXIMIZE)
 
 	#constraints
@@ -100,9 +100,9 @@ def PMR_ws(x,y,P):
 		m.addConstr(w[i], GRB.GREATER_EQUAL, 0, "Constraint 1_%d" % (i+1))
 		m.addConstr(w[i], GRB.GREATER_EQUAL, w[i+1], "Constraint 2_%d" % (i+1))
 	m.addConstr(w[nbvar-1], GRB.GREATER_EQUAL, 0, "Constraint 1_%d"%(nbvar+1))
-	
+
 	m.addConstr(sum(w), GRB.EQUAL, 1, "Constraint 3")
-	
+
 	print("avant for")
 	for (a,b) in P:
 		print("in for : ", a, " ", b)
@@ -111,7 +111,7 @@ def PMR_ws(x,y,P):
 		print(a_rearr, " ", b_rearr)
 		input("a_rearr, b_rearr")
 		m.addConstr(w @ a_rearr, GRB.GREATER_EQUAL, w @ b_rearr)
-		
+
 	# Resolution
 	m.optimize()
 	"""
@@ -124,7 +124,7 @@ def PMR_ws(x,y,P):
 	"""
 
 	return m.getObjective().getValue()
-	
+
 if __name__ == '__main__':
 	X = np.array([[45,32], [21,3], [5,90]])
 	print(main_(X))
